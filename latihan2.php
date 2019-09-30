5fd68f68841a18ab19c1a835bd37302498ae1f3c
deltariska <55369814+deltariska@users.noreply.github.com>
2019-09-30 14:49:24 +0700

    Add files via upload

---
+++ latihan2.php
@@ -0,0 +1,19 @@
+<?php
+
+//latihan 2
+
+echo "GANJIL";
+echo "<br>";
+for ($ganjil=1; $ganjil<100; $ganjil+=2){
+	echo $ganjil;
+	echo "<br>";
+}
+
+
+echo "GENAP";
+echo "<br>";
+for ($genap=0; $genap<100; $genap+=2){
+	echo $genap;
+	echo "<br>";
+}
+?>
